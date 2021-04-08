47375505b2cb55cc269cfb31811277cec6baae97
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-04-08 13:22:49 +0900

    ページを増やした

---
+++ pages/hello.js
@@ -0,0 +1,12 @@
+import { pageLink } from "./index";
+
+function Hello() {
+  return (
+    <>
+      {pageLink}
+      <h1>Hello!!</h1>
+    </>
+  );
+}
+
+export default Hello;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
-function HomePage() {
+import Link from "next/link";
+
+function Welcome() {
   return <div>Welcome to Next.js!</div>;
+}
+
+export const pageLink = (
+  <ul>
+    <li>
+      <Link href="/">
+        <a>Welcome</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/hello">
+        <a>Hello</a>
+      </Link>
+    </li>
+  </ul>
+);
+
+function HomePage() {
+  return (
+    <>
+      {pageLink}
+      <Welcome />
+    </>
+  );
 }
 
 export default HomePage;
